7721f16a3e391d146eff27d4478663698adb9bac
rotwang00 <paulthurmond@gmail.com>
2022-08-02 09:47:09 -0500

    Complete all exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    titles = books.map(books => books.title);
+    return titles;
 };
+
+// Sample Array
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,49 @@
-const findTheOldest = function() {
+// const findTheOldest = function(people) {
+//     const curentYear = new Date().getFullYear();
+//     const oldest = people.reduce(function(a, b) {
+//         const ageA = ('yearOfDeath' in a) ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+//         const ageB = ('yearOfDeath' in b) ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
+//         console.log(ageA, ageB);
+//         if (ageA > ageB) return a;
+//         return b
+//         })
+// };
 
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })    
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+    
+};
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
